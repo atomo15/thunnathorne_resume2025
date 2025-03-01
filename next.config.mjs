263f4ab5ca2e0bb7b7c65542c35a394d6e3e860a
Thunnathorne Synhiranakkrakul <49804761+atomo15@users.noreply.github.com>
2025-03-01 03:04:17 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ตั้งค่า Static Export
+  output: 'next export', // ตั้งค่า Static Export
   images: {
     unoptimized: true, // ปิด Image Optimization สำหรับ Static Export
   },
