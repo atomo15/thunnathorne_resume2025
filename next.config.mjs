d5e988d8a66aaaa7091a1ac7a5f8a04c64acbcbd
Thunnathorne Synhiranakkrakul <49804761+atomo15@users.noreply.github.com>
2025-03-01 02:11:14 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // ตั้งค่า Static Export
   images: {
-    unoptimized: true, // ปิด Image Optimization
+    unoptimized: true, // ปิด Image Optimization สำหรับ Static Export
   },
 };
 
